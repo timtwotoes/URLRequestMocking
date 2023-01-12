2eb96f2063efeb4178fc43fdf71ef93992183274
Tim Wolff <timtwotoes@me.com>
2023-01-12 20:49:37 +0100

    Add mock property to URLRequest

--- Sources/URLRequestMocking/URLRequestMocking.swift
+++ Sources/URLRequestMocking/URLRequestMocking.swift
@@ -1,6 +1,18 @@
-public struct URLRequestMocking {
-    public private(set) var text = "Hello, World!"
+//
+//  URLRequestMocking.swift
+//
+//
+//  Created by Tim Wolff on 08/01/2023.
+//
 
-    public init() {
-    }
+import Foundation
+
+/// Implementor of this protocol, must return a response based on the given request.
+public protocol URLRequestMocking {
+    
+    /// Produces a response for a given request. If no response is returned, a default response will be returned to URLSession.
+    /// - Parameter request: An URL request.
+    /// - Returns: Returns a simulated response from server or nil.
+    /// - Throws: Can throw an error to simulate network error conditions.
+    func response(for request: URLRequest) throws -> (response: URLResponse, data: Data)?
 }
