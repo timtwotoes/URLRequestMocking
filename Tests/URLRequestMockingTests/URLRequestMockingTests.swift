4ddd483b5f755c1cfd7ef9ff22251dbb36226c32
Tim Wolff <timtwotoes@me.com>
2023-01-12 21:35:19 +0100

    Handle mocked requests through URLProtocol

--- Tests/URLRequestMockingTests/URLRequestMockingTests.swift
+++ Tests/URLRequestMockingTests/URLRequestMockingTests.swift
@@ -6,6 +6,5 @@ final class URLRequestMockingTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(URLRequestMocking().text, "Hello, World!")
     }
 }
