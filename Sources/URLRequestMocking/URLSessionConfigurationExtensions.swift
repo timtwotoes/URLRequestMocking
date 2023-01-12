4930d47e65c928cd1c2b2b1553a01ef117ccdc74
Tim Wolff <timtwotoes@me.com>
2023-01-12 21:49:12 +0100

    Add documentation to mock creation method on URLSessionConfiguration

--- Sources/URLRequestMocking/URLSessionConfigurationExtensions.swift
+++ Sources/URLRequestMocking/URLSessionConfigurationExtensions.swift
@@ -8,6 +8,10 @@
 import Foundation
 
 extension URLSessionConfiguration {
+    
+    /// Creates a mock based on the given ``URLSessionConfiguration``
+    /// - Parameter configuration: an ``URLSessionConfiguration``
+    /// - Returns: A mocked ``URLSessionConfiguration``
     public class func mock(for configuration: URLSessionConfiguration) -> URLSessionConfiguration {
         configuration.protocolClasses = [URLProtocolMock.self]
         return configuration
