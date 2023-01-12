44c2ced9d358d40b2e8e0025a0925d87040fb659
Tim Wolff <timtwotoes@me.com>
2023-01-12 23:32:03 +0100

    Create mock draft and update test case

--- Tests/URLRequestMockingTests/URLRequestMockingTests.swift
+++ Tests/URLRequestMockingTests/URLRequestMockingTests.swift
@@ -2,9 +2,14 @@ import XCTest
 @testable import URLRequestMocking
 
 final class URLRequestMockingTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+    func testMock() async throws {
+        let session = URLSession(configuration: .mock(for: .ephemeral))
+        var request = URLRequest(url: URL(string: "https://example.com")!)
+        request.mock = URLRequestMock { request in
+            URLResponse(url: URL(string: "https://example.com")!, mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
+            Data()
+        }
+        
+        let result = try await session.data(for: request)
     }
 }
