f8ef18d031083d1a41874f8e1b0dac0c42438c9b
Tim Wolff <timtwotoes@me.com>
2023-01-27 01:11:49 +0100

    Add documentation to public API

--- Sources/URLRequestMocking/URLSessionConfigurationExtensions.swift
+++ Sources/URLRequestMocking/URLSessionConfigurationExtensions.swift
@@ -10,6 +10,33 @@ import Foundation
 extension URLSessionConfiguration {
     
     /// Creates a mock based on the given ``URLSessionConfiguration``
+    ///
+    /// All URLRequests is intercepted when an URLSession uses a mocked URLSessionConfiguration.
+    /// There are two possible configurations. A configuration that only looks for mocks attached to URLRequests,
+    /// and a configuration that uses a _global_ mock that is attached to the URLSessionConfiguration.
+    ///
+    /// A configuration that only looks for mocks attached to URLRequests.
+    ///
+    /// ````
+    /// let session = URLSession(configuration: .mock(for: .ephemeral))
+    /// var request = URLRequest(url: URL(string: "https://apple.com")!)
+    /// request.mock = URLRequestMock { request in
+    ///     return nil
+    /// }
+    /// ````
+    ///
+    /// A configuration that uses a _global_ mock that is attached to the URLSessionConfiguration.
+    ///
+    /// ````
+    /// let mock = URLRequestMock { request in
+    ///     return nil
+    /// }
+    ///
+    /// let session = URLSession(configuration: .mock(for: .ephemeral), using: mock)
+    /// ````
+    ///
+    /// The _global_ mock can be overriden assigning a mock to an URLRequest.
+    ///
     /// - Parameter configuration: an ``URLSessionConfiguration``
     /// - Returns: A mocked ``URLSessionConfiguration``
     public class func mock(for configuration: URLSessionConfiguration, using mock: (any URLRequestMocking)? = nil) -> URLSessionConfiguration {
